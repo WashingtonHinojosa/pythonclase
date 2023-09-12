75ef284fabb408a5d500679d7d1b401fa8e0b678
WashingtonHinojosa <144399875+WashingtonHinojosa@users.noreply.github.com>
2023-09-11 21:10:44 -0500

    Add files via upload

---
+++ info_personal.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 20:07:08 2023
+
+@author: BestSystem
+"""
+
+nombre=input("ingrese nombre:")
+print(nombre)
+salario=float(input("Ingrese sueldo:"))
+print(salario)
+cedula=input("Su cedula es:")
+print(cedula)
+
+first_name=input("Mi nombre es:")
+print(first_name)
+last_name=input("y mi apellido:")
+print(last_name)
+location=input("Vivo en:")
+print(location)
+age=int(input("mi edad es:"))
+print(age)
+spacio=" "
+
+print("Mi nombre es",spacio,first_name,spacio,last_name,spacio,"mi residencia es en",
+      location,spacio,"tengo",age,spacio,"anios")
