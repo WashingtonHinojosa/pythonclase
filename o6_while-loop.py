643cfd6121dfed9756673ab4f48bf7825ca1db85
WashingtonHinojosa <144399875+WashingtonHinojosa@users.noreply.github.com>
2023-09-19 00:24:16 -0500

    Add files via upload

---
+++ o6_while-loop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 00:23:03 2023
+
+@author: BestSystem
+"""
+
+x=input("Enter a number to count to: ")
+x=int(x)
+y=1
+while y<=x:
+print(y)
+y=y+1
